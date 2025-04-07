19f8cba50aa338719be9c70041ceee7a2b71b723
KirillMurtazin <kirillmurtazin24@gmail.com>
2025-04-07 14:39:48 +0300

    Задание 2

---
+++ Task2sem2/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <locale.h>
+#include "Task2.h"
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    // Создаем три точки для треугольника
+    Point A(0.0, 0.0);
+    Point B(3.0, 0.0);
+    Point C(0.0, 4.0);
+
+    // Создаем треугольник
+    Triangle triangle(A, B, C);
+
+    // Вычисляем периметр и площадь
+    double perimeter = triangle.calculatePerimeter(triangle);
+    double area = triangle.calculateArea(triangle);
+
+    // Выводим результаты
+    std::cout << "Периметр треугольника: " << perimeter << std::endl;
+    std::cout << "Площадь треугольника: " << area << std::endl;
+
+    return 0;
+}
